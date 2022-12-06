96e01f761c1232e6de39bb36463ecbfa4890cd7a
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-12-06 22:44:38 +0200

    implemented api

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,15 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "nanoid";
 import { NotificationManager } from "react-notifications";
 import initialContacts from 'constants/contact';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
+import { fetchContacts } from './operations';
 
 
-const contactsInitialState = { value: initialContacts };
+// const contactsInitialState = { value: initialContacts };
 
 export const contactsSlice = createSlice({
     name: "contacts",
-    initialState: contactsInitialState,
+    initialState: {
+        items: [],
+        isLoading: false,
+        error: null
+    },
     reducers: {
         addContacts(state, action) {
             const { name, number } = action.payload;
@@ -29,15 +32,25 @@ export const contactsSlice = createSlice({
             };
         }
     },
+    extraReducers: {
+        [fetchContacts.pending](state) {
+            state.isLoading = true;
+        },
+        [fetchContacts.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.items = action.payload;
+        },
+        [fetchContacts.rejected](state, action) {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+    },
 });
 
-const persistConfig = {
-    key: 'root',
-    storage,
-}
 
-export const persistedContactsReducer = persistReducer(persistConfig, contactsSlice.reducer)
+
 
 export const { addContacts, deleteContact } = contactsSlice.actions;
 
-export const getContacts = state => state.contacts.value
+export const getContacts = state => state.contacts
